6beff9eede4abeaf1ad211fcda7f5bd4617aaa8f
arkaim92 <arkaim92@gmail.com>
2022-08-01 16:44:45 +0300

    php8 added changes
    php8 added changes

---
+++ src/Transaction/Mutator/SPLFixedArrayPHP8.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BitWasp\Bitcoin\Transaction\Mutator;
+
+class SPLFixedArrayPHP8 implements Iterator {
+    private $keyMask;
+    private $values;
+    private $index;
+
+    public function __construct(int $digits = 1) {
+        $this->values = new SPLFixedArray($digits);
+        $this->keyMask = "%0{$digits}s";
+        $this->index = 0;
+    }
+
+    public function rewind(): void {
+        $this->index = 0;
+    }
+
+    public function current(): int {
+        return $this->values[$this->index] ?? 0;
+    }
+
+    public function key(): string {
+        return sprintf($this->keyMask, $this->index);
+    }
+
+    public function next(): void {
+        ++$this->index;
+    }
+
+    public function valid(): bool {
+        return $this->index < $this->values->count();
+    }
+}
+
+?>
